bd6b5bd2a2ec27180cd42e51589866faa1916045
Sulaymane.Dagnet <sulaymane.dagnet@insa-rennes.fr>
2025-03-29 18:27:22 +0100

    initial repo

---
+++ README.md
@@ -0,0 +1,44 @@
+# TANK IA
+IA qui jour au jeu tank [Tank](https://en.wikipedia.org/wiki/Tank_(video_game))
+
+Modèle d'IA : Q Learning apprentissage par renforcement 
+
+## Jeu : 
+Deux (ou plus) s'affrontent dans une arène composé de murs. Les tanks peuvent tirer des missiles pour abbattre leur ennemi.
+
+### Mécaniques : 
+Mouvements : 
+- Tourner dans le sens des aiguilles d'une montre, D_KEY
+- Tourner dans le sens inverse des aiguilles d'une montre, Q_KEY
+- Avancer : Z_KEY
+- Reculer : S_KEY
+
+Il n'est possible de faire qu'une action à la fois
+
+## Entrainement : 
+Deux agents jouent sur une carte aléatoire (seed) et doivent s'affronter, ils ont 3 vies. 
+Les agents sont récompensés ou punis en fonctions de leurs actions : 
+
+Actions positives +: 
++ Abbattre un ennemi
++ Finir un ennemi 
++ Survivre (temps) 
++ Eviter une balle 
+
+Actions négatives -: 
+- Mourir
+- Perdre la partie 
+- Toucher un mur 
+
+# Etapes de développement : 
+1. développer le jeu en lui même 
+    a. Créer un controlleur joueur qui modifie un environnement
+    b. Créer un viewer graphique pour représenter le jeu
+
+
+
+### Idées bonus 
+- différents terrains : boue, eau, arbres... qui ralentissent
+- mines 
+- shield
+- dash multidirectionnel pour éviter les balles 
---
+++ src/graphic_viewer.cpp
@@ -0,0 +1,131 @@
+/**
+ * Se charge de l'affichage graphique de l'environnement
+ * Basé sur SDL2
+ */
+
+#include "graphic_viewer.hpp"
+
+bool init(SDL_Window* & gW, SDL_Renderer* & gR, SDL_Surface* & gSS)
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gW = SDL_CreateWindow( "Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gW == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			gR = SDL_CreateRenderer(gW, -1,SDL_RENDERER_ACCELERATED);
+			if(gR == NULL){
+				printf( "Renderer could not be initialized! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else{
+				// init TTF 
+				if(TTF_Init() < 0){
+					printf( "TTF could not be initialized! TTF Error: %s\n", TTF_GetError());
+					success = false;
+				}else{
+					printf("Everything initialized!\n");
+					gSS = SDL_GetWindowSurface(gW);
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+
+void close(SDL_Window* & gW, SDL_Renderer* & gR, TTF_Font* & font)
+{
+	SDL_DestroyRenderer(gR);
+	SDL_DestroyWindow( gW );
+
+	gW = NULL;
+	gR = NULL;
+
+	//Quit SDL subsystems
+	TTF_CloseFont(font);
+	font = NULL;
+	
+	IMG_Quit();
+	SDL_Quit();
+}
+
+SDL_Texture* loadTexture(SDL_Renderer* sR, std::string path )
+{
+	//The final optimized image
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}
+	else
+	{
+		//Convert surface to screen format
+		newTexture = SDL_CreateTextureFromSurface( sR, loadedSurface);
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	return newTexture;
+}
+
+void main(){
+    SDL_Window* gWindow = NULL;
+    SDL_Renderer* gRenderer = NULL;
+    SDL_Surface* gScreenSurface = NULL;
+    TTF_Font* gFont = NULL;
+
+    if(!init(gWindow, gRenderer, gScreenSurface)){
+        printf("Failed to initialize!\n");
+    }else{
+        bool quit = false;
+        SDL_Event e;
+
+        SDL_Texture* gTexture = loadTexture(gRenderer, "res/pong.png");
+        if(gTexture == NULL){
+            printf("Failed to load texture!\n");
+        }else{
+            SDL_Rect dest;
+            dest.x = 0;
+            dest.y = 0;
+            dest.w = 1280;
+            dest.h = 720;
+
+            SDL_RenderCopy(gRenderer, gTexture, NULL, &dest);
+            SDL_RenderPresent(gRenderer);
+        }
+
+        while(!quit){
+            while(SDL_PollEvent(&e) != 0){
+                if(e.type == SDL_QUIT){
+                    quit = true;
+                }
+            }
+        }
+    }
+
+    close(gWindow, gRenderer, gFont);
+}
